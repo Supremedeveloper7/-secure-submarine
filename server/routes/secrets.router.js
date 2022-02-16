3aa83e961409f601bddf82f83f5ffafb0ababba5
Supreme Supreme <supremesupreme@Supremes-MacBook-Pro.local>
2022-02-16 13:23:32 -0600

    finshed base requirements

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {rejectUnauthenticated} = require('../modules/authentication-middleware')
 
-router.get('/', (req, res) => {
-  // what is the value of req.user????
-  console.log('req.user:', req.user);
-
+router.get('/', rejectUnauthenticated,(req, res) => {
+console.log('req.user:', req.user); 
+req.user.clearance_level
+let queryText = `select * from "secret"
+where "secrecy_level" <= $1;`;
   pool
-    .query(`SELECT * FROM "secret";`)
+    .query(queryText, [req.user.clearance_level])
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
       res.sendStatus(500);
     });
 });
+
 
 module.exports = router;
